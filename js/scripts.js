ec3f70db980b136b12f09169e1f1f4867b0f6e8b
Paul Wandrey <88623843+Kodeiak@users.noreply.github.com>
2022-03-16 11:28:13 -0500

    added loop to print list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,11 @@ let pokemonList = [
   }
 ];
 
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+  if (pokemonList[i].height > .6) {
+    document.write(' - Wow, that\'s big!<br>');
+  } else {
+    document.write('<br>');
+  }
+}
